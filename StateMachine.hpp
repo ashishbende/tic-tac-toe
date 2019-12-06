444e2fc7c5ac4fd850447e9893e5d5541fbfd85c
Ashish Bende <ashishbende@cnollege.net>
2019-12-06 07:17:23 -0800

    Adds basic state machine code

---
+++ StateMachine.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "State.hpp"
+#include <memory>
+#include <stack>
+
+namespace TicTacToe{
+    typedef std::unique_ptr<State> StateRef;
+
+    class StateMachine{
+        public:
+        StateMachine( ) { }
+        ~StateMachine( ) { }
+
+        void AddState(StateRef newState, bool isReplacing = true);
+        void RemoveState( );
+
+        void ProcessStateChanges( );
+
+        StateRef& GetActiveState( );
+
+        private:
+        std::stack<StateRef> _states;
+        StateRef _newState;
+        bool _isRemoving;
+        bool _isAdding;
+        bool _isReplacing;
+    };
+}
